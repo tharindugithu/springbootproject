3d67b45232db666c12e0ab084e741779653c2543
tharindugithu <atharindu325@gmail.com>
2023-03-15 14:50:35 +0530

    updated

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -8,13 +8,7 @@ public class Main {
 
         Doctor d = context.getBean(Doctor.class);//get value from the class type
         d.assist();
-        System.out.println(d.getQualification());
-        //System.out.println(d.getQualification()); active line 9 in spring.xml
-        //d.getNurse().assist(); active line 10 in spring.xml
-        Nurse n = (Nurse) context.getBean("nurse");//get the value from id
-        //n.assist();
-        Staff s = context.getBean(Doctor.class);//get value from the class type
-        //s.assist();
+
 
     }
 }
