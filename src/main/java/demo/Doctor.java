796ab0406c9bc74167f25f0ffe457e076ddb0e23
tharindugithu <atharindu325@gmail.com>
2023-03-15 20:34:48 +0530

    updated

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,32 +1,11 @@
 package demo;
 
+import org.springframework.stereotype.Component;
+
+@Component // what ever the class create the beans out of it
 public class Doctor implements Staff {
-
-    private String qualification;
-//    private Nurse nurse;
-
-
-    public Doctor(String qualification) {
-        this.qualification = qualification;
-    }
 
     public void assist(){
         System.out.println("Doctor is Assisting");
     }
-
-    public String getQualification() {
-        return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }
-
-//    public Nurse getNurse() {
-//        return nurse;
-//    }
-//
-//    public void setNurse(Nurse nurse) {
-//        this.nurse = nurse;
-//    }
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,14 +1,16 @@
 package demo;
+import org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-
-        Doctor d = context.getBean(Doctor.class);//get value from the class type
+        Doctor d = context.getBean(Doctor.class);
+//        Staff s = context.getBean(Doctor.class);
+//        s.assist();
         d.assist();
-
-
     }
+
 }
